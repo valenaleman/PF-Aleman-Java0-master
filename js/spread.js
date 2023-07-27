27b7b0f40e1b1e30dc89f8d527f2158a8f90df91
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-07-27 20:45:42 -0300

    agrego elementos a spread.js

---
+++ js/spread.js
@@ -0,0 +1,13 @@
+// Spread operator
+
+// Vamos a separar un array y convertirlo en un conjunto de datos
+
+const valores = [ 20, 21, 22, 23 ];
+
+// Math.max
+console.log(Math.max(...valores))
+
+console.log(valores);
+
+console.log(...valores);
+
