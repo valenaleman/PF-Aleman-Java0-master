eee2a33ae52131e7a1c0b68e4251bc50cc352f99
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-07-30 20:30:27 -0300

    agrego sweet alert en otro jsW

---
+++ js/appdos.js
@@ -0,0 +1,10 @@
+const miBoton = document.getElementById('boton');
+
+miBoton.onclick = () => {
+    Swal.fire({
+        title: 'Esta es la dirección de la pañalera',
+        text: 'Para cerrar este cartel clickee el icono azul de abajo',
+        icon: 'info',
+        confirmButtonText: 'Clickee aqui'
+      })
+}
